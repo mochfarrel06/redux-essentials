946425434dc536cf101f4122d0eace9b30f9dc19
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-10 17:21:32 +0700

    adding layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import HomePage from "./pages/home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import {Navbar} from "../app/Navbar";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <div className="app">
+        <section>
+          <h2>Welcome to the Redux Essentials example app!</h2>
+        </section>
+      </div>
+    </>
+  );
+}
