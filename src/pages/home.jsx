630fdbc24f3990d90f78c805f32c0602e56394dc
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-11 11:19:30 +0700

    add new post entries to the Redux store.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import Layouts from "../components/layouts/Layout";
+import {AddPostForm} from "../features/posts/AddPostForm";
 import {PostsList} from "../features/posts/PostsList";
 
 export default function HomePage() {
   return (
     <Layouts>
+      <AddPostForm />
       <PostsList />
     </Layouts>
   );
