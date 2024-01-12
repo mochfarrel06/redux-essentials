1cc3f16c7f89c9d03e77061f7cbf138ee93c78a8
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-12 10:47:49 +0700

    Adding the Single Post Route

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
 
-export const SinglePostPage = ({match}) => {
-  const {postId} = match.params;
+export const SinglePostPage = () => {
+  const {postId} = useParams();
 
   const post = useSelector((state) =>
-    state.posts.find((post) => post.id === postId)
+    state.posts.find((post) => post.id === Number(postId))
   );
 
   if (!post) {
     return (
       <section>
-        <h2>Post not found</h2>
+        <h2>Post not found!</h2>
       </section>
     );
   }
